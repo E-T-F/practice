2612b572d0cd5bfbbfd7057ce830b581cb5f21d4
zhangfei1 <zhangfei1@corp.netease.com>
2020-12-24 00:34:05 +0800

    sort color

--- test/src/main/java/ds/array/basic/classify/sortColors.java
+++ test/src/main/java/ds/array/basic/classify/sortColors.java
@@ -1,5 +1,7 @@
 package ds.array.basic.classify;
 
+import java.util.Arrays;
+
 /**
  * 颜色分类
  * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
@@ -12,10 +14,31 @@ package ds.array.basic.classify;
 public class sortColors {
 
     public static void main(String[] args) {
+        int[] nums = {2,0,1};
+        sortColors(nums);
+
+        System.out.println(Arrays.toString(nums));
+
 
     }
 
-    public void sortColors(int[] nums) {
+    public static void sortColorsForPartition(int[] nums) {
+        int left = 0;
+        int right = nums.length;
+        int i = 0;
 
+        while (i < right) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                left++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else if (nums[i] == 2) {
+                right--;
+                swap(nums, i, right);
+            }
+        }
     }
+
 }

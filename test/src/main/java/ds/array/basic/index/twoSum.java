f8c6a142bef0c78eec06ee6cde2f8207a2c5ba01
zhangfei <13337000287@163.com>
2021-01-03 22:14:04 +0800

    two sum

---
+++ test/src/main/java/ds/array/basic/index/twoSum.java
@@ -0,0 +1,74 @@
+package ds.array.basic.index;
+
+import java.util.Arrays;
+
+/**
+ * 两数之和 II - 输入有序数组
+ * 给定一个已按照升序排列的有序数组，找到两个数使得它们相加之和等于目标数。
+ * <p>
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1必须小于index2。
+ * <p>
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * <p>
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/all-about-array/x9i1x6/
+ */
+public class twoSum {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 7, 11, 15};
+        int[] res = twoSum2(numbers, 9);
+        System.out.println(Arrays.toString(res));
+
+    }
+
+
+    public static int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[]{i + 1, j + 1};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static int[] twoSum1(int[] numbers, int target) {
+        int index1 = 0;
+        int index2 = numbers.length - 1;
+        while (numbers[index1] + numbers[index2] != target) {
+            if (numbers[index1] + numbers[index2] < target) {
+                index1++;
+            }
+
+            if (numbers[index1] + numbers[index2] > target) {
+                index2--;
+            }
+        }
+        return new int[]{index1 + 1, index2 + 1};
+    }
+
+    public static int[] twoSum2(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            int low = i + 1;
+            int high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return null;
+    }
+}

eb9a4ede809f055e458056bf13dd6cb4d6dfa5a4
zhangfei1 <zhangfei1@corp.netease.com>
2021-01-07 23:14:18 +0800

    minSubArrayLen

--- test/src/main/java/ds/array/basic/index/MinSubArrayLen.java
+++ test/src/main/java/ds/array/basic/index/MinSubArrayLen.java
@@ -21,13 +21,14 @@ package ds.array.basic.index;
 public class MinSubArrayLen {
 
     public static void main(String[] args) {
-//        int s = 11;
+        int s = 11;
         int[] nums = {1, 2, 3, 4, 5};
-        int s = 15;
+//        int s = 15;
 //        int[] nums = {2,3,1,2,4,3};
-        System.out.println(minSubArrayLen(s, nums));
+        System.out.println(minSubArrayLen3(s, nums));
     }
 
+    //# 维护一个滑动窗口nums[i,j], nums[i...j] < s
     public static int minSubArrayLen(int s, int[] nums) {
 
         if (nums.length == 0) {
@@ -53,8 +54,85 @@ public class MinSubArrayLen {
     }
 
 
+    public static int minSubArrayLen2(int s, int[] nums) {
+
+        int len = nums.length;
+        int result = Integer.MAX_VALUE;
+
+        for (int i = 0; i < len; i++) {
+            int sum = 0;
+            for (int j = i; j < len; j++) {
+                sum += nums[j];
+                if (sum >= s) {
+                    result = Math.min(result, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+
+
+    /**
+     * 正整数数组，求前缀和（有序），然后通过二分查找
+     *
+     * @param s
+     * @param nums
+     * @return
+     */
+    public static int minSubArrayLen3(int s, int[] nums) {
+
+        int len = nums.length;
+
+
+        int result = Integer.MAX_VALUE;
+        int[] preSums = new int[len + 1];
+        preSums[0] = 0;
+
+        for (int i = 1; i <= len; i++) {
+            preSums[i] = preSums[i - 1] + nums[i - 1];
+        }
+
+        for (int i = 1; i <= len; i++) {
+            int target = s + preSums[i - 1];
+            int bound = binarySearchVal(preSums, target);
+//            int bound = Arrays.binarySearch(preSums, target);
+            if (bound < 0) {
+                bound = -bound - 1;
+            }
+            if (bound <= len) {
+                result = Math.min(result, bound - i + 1);
+            }
+        }
+
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+
+    private static int binarySearchVal(int[] preSums, int target) {
+        int left = 0;
+        int right = preSums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            int midVal = preSums[mid];
+
+            if (midVal < target) {
+                left = mid + 1;
+            } else if (midVal > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -(left + 1);
+    }
+
+
     /**
      * 取等于s的，待验证
+     *
      * @param s
      * @param nums
      * @return

5cdda7ea14c5dd9b6d799730cc4e638b00dff85e
zhangfei1 <zhangfei1@corp.netease.com>
2021-01-23 10:19:38 +0800

    flatten alg edsc

--- test/src/main/java/ds/linkedlist/Flatten.java
+++ test/src/main/java/ds/linkedlist/Flatten.java
@@ -1,10 +1,23 @@
 package ds.linkedlist;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
+ * 扁平化多级双向链表
+ * 多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
+ * <p>
+ * 给你位于列表第一级的头节点，请你扁平化列表，使所有结点出现在单级双链表中。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+ * 输出：[1,2,3,7,8,11,12,9,10,4,5,6]
+ * <p>
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/linked-list/fw8v5/
+ *
  * @Auther: etf
  * @Date: 2021-01-23 09:07
  * @Description:
@@ -34,7 +47,6 @@ public class Flatten {
         System.out.println(head);
 
 
-
         System.out.println(node1);
 
     }
@@ -62,7 +74,6 @@ public class Flatten {
         return head;
     }
 }
-
 
 
 class ChildNode {

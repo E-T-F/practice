15e2cecb7bd804c49efe8534d5a92843f22c9791
zhangfei <13337000287@163.com>
2021-03-10 09:23:37 +0800

    levelOrder2

---
+++ test/src/main/java/alg/SwordOffer/LevelOrder2.java
@@ -0,0 +1,67 @@
+package alg.SwordOffer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ * <p>
+ * <p>
+ * <p>
+ * 例如:
+ * 给定二叉树:[3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * <p>
+ * 提示：
+ * <p>
+ * 节点总数 <= 1000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LevelOrder2 {
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<List<Integer>> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
